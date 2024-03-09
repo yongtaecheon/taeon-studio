80b5036359f6c25eb1ad8d5d4232f861545ad8ae
yongtaecheon <ide2143@naver.com>
2024-03-10 04:03:31 +0900

    about page updateD

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,16 +7,39 @@ export default function About() {
     <div className={styles.about}>
       <h1>About</h1>
       <img className={styles.img} src={img1} alt="my img"></img>
-      <div>
-        <h2>Music Careers</h2>
-        <p><a href="https://www.youtube.com/@tae_on">Taeon</a>&nbsp;&nbsp;
-          <a href="https://www.youtube.com/@arbor_official">Arbor</a>&nbsp;&nbsp;
-          <a href="https://www.youtube.com/@vieve1784">Vieve</a>&nbsp;&nbsp;
+      <div className={styles.content}>
+        <h1>Music Careers</h1>
+        <p className={styles.link}><a href="https://www.youtube.com/@tae_on">Taeon</a><br/>
+          <a href="https://www.youtube.com/@arbor_official">Arbor</a><br/>
+          <a href="https://www.youtube.com/@vieve1784">Vieve</a>
+        </p>
+        <h1>Gears</h1>
+        <p>
+          <strong>· DAW:</strong><br/>
+          - Logic Pro X<br />
+          <strong>· Guitar:</strong><br />
+          - Fender Japan 72 Stratocaster<br />
+          - Fender Japan 62 Telecaster<br />
+          - Epiphone by Gibson ES-335<br />
+          <strong>· Effects:</strong><br />
+          - Digitech Whammy 5<br />
+          - Fulltone OCD<br />
+          - Strymon Riverside<br />
+          - UAFX Dream 65<br />
+          - Walrus Audio Julia<br />
+          - Walrus Audio ARP-87<br />
+          - Strymon Bluesky<br />
+          - Strymon Ojai<br />
+          <strong>· Audio Interface:</strong><br/>
+          - SSL 2<br />
+          <strong>· Recording Gears:</strong><br/>
+          - Shure SM 57<br />
+          - Shure SRH 440<br />
         </p>
       </div>
       <footer className={styles.footer}>
         <p>&copy; 천용태 Yongtae Cheon - 태온 taeon</p>
-        <p>Source code opens on <a className={styles.footer_a} href="https://github.com/yongtaecheon/taeon-studio">here</a></p>
+        <p>Page source code opens on <a className={styles.footer_a} href="https://github.com/yongtaecheon/taeon-studio">here</a></p>
         <a className={styles.footer_icon} href="https://www.youtube.com/@tae_on"><i className="bi bi-youtube"></i></a>
         <a className={styles.footer_icon} href="https://soundcloud.com/tae-on"><i className="bi bi-cloud-fill"></i></a>
         <a className={styles.footer_icon} href="https://github.com/yongtaecheon"><i className="bi bi-github"></i></a>
